19dc20d59be64860002f77977302e7dfc778d2cb
Kierandoolan <kierandoolan3@gmail.com>
2023-11-29 15:45:49 +0000

    add functionality for player1 dice

--- Dicee Challenge - Starting Files/index.js
+++ Dicee Challenge - Starting Files/index.js
@@ -1,7 +1,9 @@
-var randomNumber1 = Math.floor(Math.random() * ) + 1;
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
 
-var randomDiceImage = "Dice" + randomNumber1 + ".png";
+var randomDiceImage = "dice" + randomNumber1 + ".png";
 
 var randomImageSource = "images/" + randomDiceImage;
 
-var image1 = document.querySelectorAll("img")[0]
+var image1 = document.querySelectorAll("img")[0];
+
+image1.setAttribute('src', randomImageSource);
